0b93d48924d2dde0b42716891296f4723b5e7d37
shauritanga <curtisisaac36@gmail.com>
2025-08-18 22:16:04 +0300

    Initial Commit

---
+++ HeartScanner/HeartScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartScannerApp.swift
+//  HeartScanner
+//
+//  Created by Athanas Shauritanga on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HeartScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeartScannerTests/HeartScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeartScannerTests.swift
+//  HeartScannerTests
+//
+//  Created by Athanas Shauritanga on 18/08/2025.
+//
+
+import Testing
+@testable import HeartScanner
+
+struct HeartScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
